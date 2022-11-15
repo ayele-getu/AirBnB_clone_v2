3687e59a790bcbfd03870dd79b827bc953189eb9
ayele-getu <ayelegetu44@gmail.com>
2022-11-15 14:24:58 +0300

    Air BnB clone --version 2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""start flask and set a route"""
-
+""" Script that runs a Flask app """
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close(self):
-    """Closes sessions"""
+def closing(error):
+    """closes session """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def state_list():
-    """states"""
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
+def states():
+    """ function that returns text"""
+    state = storage.all()
+    return render_template('7-states_list.html', state=state)
 
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
